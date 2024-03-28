d9080504011207610598a1440aa03acc23f341e5
RaynoRYN <1184224658@qq.com>
2024-03-28 17:31:01 +0800

    Third_commit basic_game_finished

--- app/src/main/java/com/example/truly_myfirst_application/ChoosingLayoutActivity.java
+++ app/src/main/java/com/example/truly_myfirst_application/ChoosingLayoutActivity.java
@@ -1,15 +1,15 @@
 package com.example.truly_myfirst_application;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class ChoosingLayoutActivity extends AppCompatActivity {
 
@@ -20,23 +20,73 @@ public class ChoosingLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.choosing_layout);
 
 
-        Button button = (Button) findViewById(R.id.level1);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button1 = (Button) findViewById(R.id.level1);
+        button1.setOnClickListener(v -> {
+            Intent intent_to_level1 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
+            intent_to_level1.putExtra("level_label", 1);
+            startActivity(intent_to_level1);
+        });
+
+        Button button2 = (Button) findViewById(R.id.level2);
+        button2.setOnClickListener(v -> {
+            Intent intent_to_level1 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
+            intent_to_level1.putExtra("level_label", 2);
+            startActivity(intent_to_level1);
+        });
+
+        Button button3 = (Button) findViewById(R.id.level3);
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_to_level1 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
+                intent_to_level1.putExtra("level_label", 3);
                 startActivity(intent_to_level1);
             }
         });
 
-//        Button button2 = (Button) findViewById(R.id.level2);
-//        button2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent_to_level2 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
-//                startActivity(intent_to_level2);
-//            }
-//        });
+        Button button4 = (Button) findViewById(R.id.level4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_to_level1 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
+                intent_to_level1.putExtra("level_label", 4);
+                startActivity(intent_to_level1);
+            }
+        });
 
+        Button button5 = (Button) findViewById(R.id.level5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_to_level1 = new Intent(ChoosingLayoutActivity.this, Level1_Activity.class);
+                intent_to_level1.putExtra("level_label", 5);
+                startActivity(intent_to_level1);
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.level6);
+        button6.setOnClickListener(v -> {
+            Toast.makeText(ChoosingLayoutActivity.this, "别点了，没做呢", Toast.LENGTH_SHORT).show();
+            button6.setBackgroundColor(Color.GRAY);
+            button6.setEnabled(false);
+        });
+
+        Button button7 = (Button) findViewById(R.id.level7);
+        button7.setOnClickListener(v -> {
+            button7.setBackgroundColor(Color.GRAY);
+            button7.setEnabled(false);
+        });
+
+        Button button8 = (Button) findViewById(R.id.level8);
+        button8.setOnClickListener(v -> {
+            button8.setBackgroundColor(Color.GRAY);
+            button8.setEnabled(false);
+        });
+
+        Button button9 = (Button) findViewById(R.id.level9);
+        button9.setOnClickListener(v -> {
+            button9.setBackgroundColor(Color.GRAY);
+            button9.setEnabled(false);
+        });
     }
 }
--- app/src/main/java/com/example/truly_myfirst_application/Level1_Activity.java
+++ app/src/main/java/com/example/truly_myfirst_application/Level1_Activity.java
@@ -1,17 +1,13 @@
 package com.example.truly_myfirst_application;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
 
 public class Level1_Activity extends AppCompatActivity {
 
@@ -45,81 +41,126 @@ public class Level1_Activity extends AppCompatActivity {
         two_dim_views[3][3] = findViewById(R.id.button4_4);
 
         boolean[][] views_value = new boolean[4][4];
+
+        Intent intent_to_level1 = getIntent();
+        int level_label = intent_to_level1.getIntExtra("level_label", 1);
+
+        switch(level_label) {
+            case 2:
+                two_dim_views[1][1].setBackgroundColor(Color.RED);
+                two_dim_views[1][2].setBackgroundColor(Color.RED);
+                two_dim_views[2][1].setBackgroundColor(Color.RED);
+                two_dim_views[2][2].setBackgroundColor(Color.RED);
+                views_value[1][1] = true;
+                views_value[1][2] = true;
+                views_value[2][1] = true;
+                views_value[2][2] = true;
+                break;
+            case 3:
+                two_dim_views[0][3].setBackgroundColor(Color.RED);
+                views_value[0][3] = true;
+                two_dim_views[2][1].setBackgroundColor(Color.RED);
+                views_value[2][1] = true;
+                break;
+            case 4:
+                two_dim_views[0][0].setBackgroundColor(Color.RED);
+                two_dim_views[0][3].setBackgroundColor(Color.RED);
+                two_dim_views[3][0].setBackgroundColor(Color.RED);
+                two_dim_views[3][3].setBackgroundColor(Color.RED);
+                views_value[0][0] = true;
+                views_value[0][3] = true;
+                views_value[3][0] = true;
+                views_value[3][3] = true;
+                break;
+            case 5:
+                Random rand_0or1 = new Random();
+                for(int i = 0; i < 4; i++) {
+                    for(int j = 0; j < 4; j++) {
+                        if(rand_0or1.nextInt(2) == 1){
+                            two_dim_views[i][j].setBackgroundColor(Color.RED);
+                            views_value[i][j] = true;
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
         final boolean[] flag = {false};
 
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 4; j++) {
                 int finalI = i;
                 int finalJ = j;
-                two_dim_views[i][j].setOnClickListener(new View.OnClickListener() {
-                    @SuppressLint("ResourceAsColor")
-                    @Override
-                    public void onClick(View v) {
-                        if(!views_value[finalI][finalJ]) two_dim_views[finalI][finalJ].setBackgroundColor(Color.RED);
-                        else two_dim_views[finalI][finalJ].setBackgroundColor(Color.GRAY );
-                        views_value[finalI][finalJ] = !views_value[finalI][finalJ];
+                two_dim_views[i][j].setOnClickListener(v -> {
+                    if(!views_value[finalI][finalJ]) two_dim_views[finalI][finalJ].setBackgroundColor(Color.RED);
+                    else two_dim_views[finalI][finalJ].setBackgroundColor(Color.GRAY );
+                    views_value[finalI][finalJ] = !views_value[finalI][finalJ];
 
-                        if(finalI == 0){
+                    if(finalI == 0){
 
-                            if(!views_value[finalI +1][finalJ]) two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.GRAY);
-                            views_value[finalI +1][finalJ] = !views_value[finalI +1][finalJ];
+                        if(!views_value[finalI +1][finalJ]) two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.GRAY);
+                        views_value[finalI +1][finalJ] = !views_value[finalI +1][finalJ];
 
-                        } else if (finalI == 3){
+                    } else if (finalI == 3){
 
-                            if(!views_value[finalI -1][finalJ]) two_dim_views[finalI -1][finalJ].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI -1][finalJ].setBackgroundColor(Color.GRAY);
-                            views_value[finalI -1][finalJ] = !views_value[finalI -1][finalJ];
+                        if(!views_value[finalI -1][finalJ]) two_dim_views[finalI -1][finalJ].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI -1][finalJ].setBackgroundColor(Color.GRAY);
+                        views_value[finalI -1][finalJ] = !views_value[finalI -1][finalJ];
 
-                        } else {
+                    } else {
 
-                            if(!views_value[finalI +1][finalJ]) two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.GRAY);
-                            views_value[finalI +1][finalJ] = !views_value[finalI +1][finalJ];
+                        if(!views_value[finalI +1][finalJ]) two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI +1][finalJ].setBackgroundColor(Color.GRAY);
+                        views_value[finalI +1][finalJ] = !views_value[finalI +1][finalJ];
 
-                            if(!views_value[finalI -1][finalJ]) two_dim_views[finalI-1][finalJ].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI-1][finalJ].setBackgroundColor(Color.GRAY);
-                            views_value[finalI-1][finalJ] = !views_value[finalI-1][finalJ];
-
-                        }
-
-                        if(finalJ == 0) {
-
-                            if(!views_value[finalI][finalJ+1]) two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.GRAY);
-                            views_value[finalI][finalJ+1] = !views_value[finalI][finalJ+1];
-                        } else if (finalJ == 3) {
-
-                            if(!views_value[finalI][finalJ-1]) two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.GRAY);
-                            views_value[finalI][finalJ-1] = !views_value[finalI][finalJ-1];
-                        } else {
-
-                            if(!views_value[finalI][finalJ-1]) two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.GRAY);
-                            views_value[finalI][finalJ-1] = !views_value[finalI][finalJ-1];
-
-                            if(!views_value[finalI][finalJ+1]) two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.RED);
-                            else two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.GRAY);
-                            views_value[finalI][finalJ+1] = !views_value[finalI][finalJ+1];
-                        }
-
-                        flag[0] = true;
-                        for (int i = 0; i < 4; i++){
-                            for (int j = 0; j < 4; j++){
-                                if(!views_value[i][j]) flag[0] = false;
-                            }
-                        }
-                        if(flag[0]) {
-                            setContentView(R.layout.victory);
-                        }
+                        if(!views_value[finalI -1][finalJ]) two_dim_views[finalI-1][finalJ].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI-1][finalJ].setBackgroundColor(Color.GRAY);
+                        views_value[finalI-1][finalJ] = !views_value[finalI-1][finalJ];
 
                     }
-                });
 
+                    if(finalJ == 0) {
+
+                        if(!views_value[finalI][finalJ+1]) two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.GRAY);
+                        views_value[finalI][finalJ+1] = !views_value[finalI][finalJ+1];
+                    } else if (finalJ == 3) {
+
+                        if(!views_value[finalI][finalJ-1]) two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.GRAY);
+                        views_value[finalI][finalJ-1] = !views_value[finalI][finalJ-1];
+                    } else {
+
+                        if(!views_value[finalI][finalJ-1]) two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI][finalJ-1].setBackgroundColor(Color.GRAY);
+                        views_value[finalI][finalJ-1] = !views_value[finalI][finalJ-1];
+
+                        if(!views_value[finalI][finalJ+1]) two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.RED);
+                        else two_dim_views[finalI][finalJ+1].setBackgroundColor(Color.GRAY);
+                        views_value[finalI][finalJ+1] = !views_value[finalI][finalJ+1];
+                    }
+
+                    flag[0] = true;
+                    for (int i1 = 0; i1 < 4; i1++){
+                        for (int j1 = 0; j1 < 4; j1++){
+                            if(!views_value[i1][j1]) flag[0] = false;
+                        }
+                    }
+                    if(flag[0]) {
+                        setContentView(R.layout.victory);
+                        Button return_button = (Button) findViewById(R.id.return_button);
+                        return_button.setOnClickListener(v1 -> {
+                            finish();
+                        });
+                    }
+
+                }); //监听设置完毕
 
             }
-        }
+        } //for_end
 
-    }
-}
+    } //onCreate_end
+} //class_end
